239ec2859ce968a15b1c4234bb60fb2842ca1a90
ludacirs <ludacirs@naver.com>
2022-01-11 01:28:21 +0900

    feat: theme.js 추가 및 적용

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+
+import theme from "./lib/styles/theme";
 
 import "./index.css";
 import "./reset.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/lib/styles/theme.js
@@ -0,0 +1,11 @@
+const size = {
+  big: `991px`,
+  small: `785`,
+};
+
+const theme = {
+  desktopS: `(max-width: ${size.small})`,
+  desktopL: `(min-width: ${size.big})`,
+};
+
+export default theme;
